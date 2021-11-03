4898618296395d7ce730212f9554451bbe14b1a3
Agustin Basly <ag.basly@hotmail.com>
2021-11-03 10:03:36 -0300

    primer desafio

---
+++ primero/js/archivo.js
@@ -0,0 +1,4 @@
+alert ("¡Bienvenido!")
+const nombre = prompt ("Ingrese su nombre");
+const apellido = prompt ("Ingrese su apellido");
+alert ("Bienvenido a la web de las mejores hamburguesas del pais" + " " + nombre + " " + apellido)
